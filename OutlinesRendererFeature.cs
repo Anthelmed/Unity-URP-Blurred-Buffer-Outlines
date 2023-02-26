3c1c704c976961f2f1e3ff1684d94a360e1fab90
Anthelme Dumont <anthelme.dumont@gmail.com>
2023-02-26 22:21:02 +0100

    Update OutlinesRendererFeature.cs

--- OutlinesRendererFeature.cs
+++ OutlinesRendererFeature.cs
@@ -6,7 +6,7 @@ using UnityEngine.Serialization;
 
 namespace Anthelme.BlurredBufferOutlines
 {
-	internal class OutlinesRendererFeature : ScriptableRendererFeature
+	public class OutlinesRendererFeature : ScriptableRendererFeature
 	{
 		[Serializable]
 		public class OutlinesSettings
